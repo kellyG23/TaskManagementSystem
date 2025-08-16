30d8cc2731a0c832ae7d04f9cc43bd40ecdeb5a1
Kelly Gabot <86221756+kellyG23@users.noreply.github.com>
2025-08-16 16:51:45 +0800

    Added the Main class

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+public class Main {
+    public static void main(String[] args) {
+        // LinkedList
+        // Store tasks (w/ name description and priority)
+        LinkedList list = new LinkedList();
+
+        // insertion, reading, deletion, searching
+        list.add("A");
+        list.add("B");
+        list.read(); // [ A B ]
+        list.search("ABC"); // ABC not found
+        list.delete("A");
+        list.read(); // [ B ]
+
+        // Stack
+        // Push Pop Peek
+
+
+
+
+
+        // Queue
+        // Extends LinkedList
+        // Queue Dequeue
+    }
+}
--- src/TaskManagementSystem.java
+++ src/TaskManagementSystem.java
@@ -1,22 +1,3 @@
 public class TaskManagementSystem {
-    public static void main(String[] args) {
-        // LinkedList
-        LinkedList list = new LinkedList();
 
-        // insertion, reading, deletion, searching
-        list.add("A");
-        list.add("B");
-        list.read(); // [ A B ]
-        list.search("ABC"); // ABC not found
-        list.delete("A");
-        list.read(); // [ B ]
-
-        // Stack
-
-
-
-
-
-        // Queue
-    }
 }
