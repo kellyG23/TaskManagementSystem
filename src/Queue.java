7df004e598706ebf4f0c0613b648dbea3dad6b0e
ashiixai <aslama.mangondaya.cabugatan@gmail.com>
2025-08-17 23:55:25 +0800

    Update Queue.java

--- src/Queue.java
+++ src/Queue.java
@@ -1,3 +1,32 @@
 public class Queue extends LinkedList {
+    
+    public void enqueue(String name) {
+        add(name); 
+        System.out.println(name + " added to the queue");
+    }
 
+    public String dequeue() {
+        if (head == null) {
+            System.out.println("Queue is empty, cannot dequeue.");
+            return null;
+        }
+        String removedData = head.getData();
+        head = head.getNext();
+        size--;
+        System.out.println(removedData + " removed from the queue");
+        return removedData;
+    }
+
+    public String peek() {
+        if (head == null) {
+            System.out.println("Queue is empty, nothing to peek.");
+            return null;
+        }
+        System.out.println("Front of the queue: " + head.getData());
+        return head.getData();
+    }
+
+    public boolean isEmpty() {
+        return head == null;
+    }
 }
